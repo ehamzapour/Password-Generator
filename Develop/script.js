fe043cc1518bcea266b77d67700608780a1b8ea0
Elham Hamzapour <ehamzapour@gmail.com>
2022-08-18 00:25:38 -0500

    fixed error, need to complete readme

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,7 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "+", "?", "-"]
+var passwordChar = [];
 
 var lower;
 var upper;
@@ -49,24 +50,24 @@ if (lower === false && upper === false && numb === false && symb === false) {
 }
 
 if (lower) {
-  password.concat(lowerCase);
+  passwordChar = passwordChar.concat(lowerCase);
 }
 if (upper) {
-  password.concat(upperCase);
+  passwordChar = passwordChar.concat(upperCase);
 }
 if (numb) {
-  password.concat(numbers);
+  passwordChar = passwordChar.concat(numbers);
 }
 if (symb) {
-  password.concat(symbols);
+  passwordChar = passwordChar.concat(symbols);
 }
 
 let finalPassword =""
-for (let i=0; i < passLength; i++) {
-  let rng =[Math.floor(Math.random() * password.length)];
-  finalPassword += finalPassword[rng];
+for (let i = 0; i < passLength; i++) {
+  let rng =[Math.floor(Math.random() * passwordChar.length)];
+  finalPassword = finalPassword + passwordChar[rng];
 }
-return finalPassword
+return finalPassword;
 
 };
 
